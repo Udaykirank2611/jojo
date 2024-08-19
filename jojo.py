1c1a6e53f77bd556c49d9348bfa6bebb0acccc04
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-08-19 18:41:40 +0530

    Update jojo.py

--- jojo.py
+++ jojo.py
@@ -3,7 +3,7 @@ import requests
 
 st.title("Download Zip File")
 
-zip_url = "https://github.com/udaykirank2611/jojo/raw/main/cse attendance.zip"
+zip_url = "https://github.com/udaykirank2611/jojo/raw/main/cse attendence.zip"
 
 # Provide a download button
 #st.markdown(f"[Download zip file]")
